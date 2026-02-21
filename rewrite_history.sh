7cec37a91a08e261b02d797d882ddc61a2d1cd60
jaykishan <pateljk043@gmail.com>
2026-02-21 15:50:28 +0530

    chore: remove unused hackathon plan files and base vite assets

--- rewrite_history.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git filter-branch --env-filter '
-    export GIT_AUTHOR_NAME="tej0730"
-    export GIT_AUTHOR_EMAIL="tejdoshi55@gmail.com"
-    export GIT_COMMITTER_NAME="tej0730"
-    export GIT_COMMITTER_EMAIL="tejdoshi55@gmail.com"
-' --force -- --all
